5ff872697079a01e351eb53bc94368cf2c59796d
shndamit25 <70576359+shndamit25@users.noreply.github.com>
2021-06-17 22:24:33 +0530

    Update map.js

--- map.js
+++ map.js
@@ -9,22 +9,17 @@ require([
   };
 
   let iconRenderer = {
-    type: "simple",
-    symbol: {
-      type: "simple-marker",
-
-      color: [
-        0,
-        197,
-        255,
-        64
-      ],
-      size: 30,
-      xoffset: 0,
-      yoffset: 15,
-      path: "M16,3.5c-4.142,0-7.5,3.358-7.5,7.5c0,4.143,7.5,18.121,7.5,18.121S23.5,15.143,23.5,11C23.5,6.858,20.143,3.5,16,3.5z M16,14.584c-1.979,0-3.584-1.604-3.584-3.584S14.021,7.416,16,7.416S19.584,9.021,19.584,11S17.979,14.584,16,14.584z"
-
+  type: "simple",  // autocasts as new SimpleRenderer()
+  symbol: {
+    type: "simple-marker",  // autocasts as new SimpleFillSymbol()
+    size:30,
+    color: [ 255, 128, 0, 0.5 ],
+    outline: {  // autocasts as new SimpleLineSymbol()
+      width: 1,
+      color: "white"
     }
+  }
+
   }
 
   let locationLabels = {
